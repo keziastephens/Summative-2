182e56b791f77520a5f740e8cd6eaaf841fec62e
keziastephens <87955878+keziastephens@users.noreply.github.com>
2021-11-04 16:53:01 +1300

    Add files via upload

---
+++ summative 2/script.js
@@ -0,0 +1,11 @@
+console.log('linked');
+
+let locationArray = [
+    {
+        type: 'hotel',
+        location: '',
+        people: '1-2',
+        nights: 'Min 1 - Max 5',
+        price: '$157 per night'
+    },
+]
