4c48fd6c093b27423130cb4ac36a636298a83ed0
keziastephens <87955878+keziastephens@users.noreply.github.com>
2021-11-09 21:14:59 +1300

    Add files via upload

--- summative 2/script.js
+++ summative 2/script.js
@@ -2,10 +2,33 @@ console.log('linked');
 
 let locationArray = [
     {
+        id: 101,
         type: 'hotel',
-        location: '',
+        location: 'queenstown',
+        name: 'Heritage Hotel',
         people: '1-2',
         nights: 'Min 1 - Max 5',
-        price: '$157 per night'
+        price: '$157 per night',
+        image: '../img/hotels/heritage-01',
+        imageTwo: '../img/hotels/heritage-02',
+        imageThree: '../img/hotels/heritage-03',
+        imageFour: '../img/hotels/heritage-04',
+        imageFive: '../img/hotels/heritage-05',
+
+    },
+    {
+        id: 102,
+        type: 'hostel',
+        location: 'queenstown',
+        name: 'Adventure Hostel',
+        people: '1',
+        nights: 'Min 1 - Max 10',
+        price: '$30 per night',
+        image: '../img/hostels/adventure-01',
+        imageTwo: '../img/hostels/adventure-02',
+        imageThree: '../img/hostels/adventure-03',
+        imageFour: '../img/hostels/adventure-04',
+        imageFive: '../img/hostels/adventure-05',
+
     },
 ]
